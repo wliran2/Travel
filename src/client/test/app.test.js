b25ee32aa24c17491d5bb572b45b8bd56fe76f3b
liran, weisbord (lw114h) <lw114h@att.com>
2020-04-20 23:15:59 +0300

    ++

--- src/client/test/app.test.js
+++ src/client/test/app.test.js
@@ -1,6 +1,6 @@
 const { valid } = require('../js/app');
 
-describe('check the Error message', () => {
+describe('check the Valid Function', () => {
     describe('empty Place', () => {
         it('check the place is empty', () => {
             const result = valid('');
@@ -8,4 +8,16 @@ describe('check the Error message', () => {
             expect(result).toBe(false);
         });
     });
+
+    describe('empty Date', () => {
+        it('check that place is valid and date is empty', () => {
+            const result = valid('Toronto');
+
+            expect(result).toBe(false);
+        });
+    });
+
+
+
+
 });

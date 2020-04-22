cc73442e80c2832ee91837793f01519ea1de2f49
liran, weisbord (lw114h) <lw114h@att.com>
2020-04-22 19:04:11 +0300

    service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -55,9 +55,12 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        /*
-        new WorkboxPlugin.GenerateSW(),
-        */
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
     ]
 
 
